d209825e495465a966a9d69ed16e62b7863b690c
gtata <grzegorz.tata@unitygroup.com>
2021-03-29 16:19:15 +0200

    achmaro_2_props_states

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
 
 class ToDoList extends Component {
+  state = {
+    tasks: this.props.tasks,
+    draft: ''
+  }
+
+  updateDraft = (e) => {
+    this.setState({draft: e.target.value})
+  }
+
+  addToDo = () => {
+    const { tasks, draft } = this.state
+    const list = this.state.tasks
+    list.push(this.state.draft)
+    this.setState({tasks: list, draft: ''})
+  }
+
   render() {
+    const { title } = this.props
+    const { tasks, draft } = this.state
     return (
       <div>
-        <h1>{this.props.title}</h1>
+        <h1>{title}</h1>
+        {tasks.map(task => <div><p>{task}</p></div>)}
+        <input type='text' onChange={this.updateDraft} value={draft} />
+        <button onClick={this.addToDo}>Add</button>
       </div>
     )
   }
 }
 
 class App extends Component {
+  myTasks = [
+    'Record a React.js video',
+    'Go for a walk'
+  ]
   render() {
     return(
       <div>
-        <ToDoList title='gtata List' />
+        <ToDoList title='gtata List' tasks={this.myTasks} />
       </div>
     )
   }
